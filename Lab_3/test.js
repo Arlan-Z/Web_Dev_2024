f45c708e0f9331f75cf8304b70edda1da431bfae
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-02-14 23:09:40 +0600

    lab_3

---
+++ Lab_3/test.js
@@ -0,0 +1,19 @@
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayInfo() {
+        console.log(`Name: ${this.name}, Age: ${this.age}`);
+    }
+}
+
+
+const person1 = new Person("Alice", 25);
+const person2 = new Person("Bob", 30);
+
+
+person1.sayInfo();
+person2.sayInfo();
