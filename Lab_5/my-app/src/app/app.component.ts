35b4410fe9f2a221f0f05f38c36ba5ca6bbc6fac
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-02-28 23:28:30 +0600

    last

--- Lab_5/my-app/src/app/app.component.ts
+++ Lab_5/my-app/src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4'];
-  products: { id: number, name: string, category: string, likes: number}[];
+  categories = ['Food', 'Drinks', 'Sauce', 'Desert'];
+  products: { id: number, name: string, category: string, likes: number, imageUrl: string}[];
 
   selectedCategory: string | null = null;
   selectedProducts: any[] = [];
@@ -31,9 +31,26 @@ export class AppComponent {
 
   constructor(){
     this.products = [
-    { id: 1, name: 'Product 1', category: 'Category 1', likes: 0 },
-    { id: 2, name: 'Product 2', category: 'Category 1', likes: 0 },
-    { id: 3, name: 'Product 3', category: 'Category 1', likes: 0 },
-  ];
-  }
+    { id: 1, name: 'Cheese Burger', category: 'Food', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Cheeseburger-new:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 2, name: 'Donut', category: 'Food', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-toffee-apple-doughnut:product-header-desktop?wid=829&hei=455&dpr=off' },
+    { id: 3, name: 'Beef Burger', category: 'Food', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-BigTasty-feb-promo:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 4, name: 'Salad', category: 'Food', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-side-salad-jan-promo:product-header-desktop?wid=829&hei=455&dpr=off' },
+    { id: 5, name: 'Fries', category: 'Food', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Fries-Medium-2:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 6, name: 'Coffee', category: 'Drinks', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Toffee-Latte-Regular-jan-promo:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 7, name: 'Juice', category: 'Drinks', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Tropicana-Orange-Juice-2:product-header-desktop?wid=829&hei=455&dpr=off' },
+    { id: 8, name: 'MilkShake', category: 'Drinks', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Strawberry-Milkshake-Medium-feb-promo:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 9, name: 'Water', category: 'Drinks', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Water-Deep-River-Rock-500ml:product-header-desktop?wid=829&hei=455&dpr=off' },
+    { id: 10, name: 'Cola', category: 'Drinks', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Diet-Coke-Medium-2:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 11, name: 'BBQ', category: 'Sauce', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-BBQ-Dip-30g-4:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 12, name: 'Tomato', category: 'Sauce', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Tangy-Tomato-Dip-1:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 13, name: 'Garlic', category: 'Sauce', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Butter-Portion:product-header-desktop?wid=829&hei=455&dpr=off' },
+    { id: 14, name: 'Sour Cream', category: 'Sauce', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Sour-Cream-Chive-Dip-40g:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 15, name: 'Salsa', category: 'Sauce', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Salsa-Dip-for-Mozzarella-Dippers-feb-promo:product-header-desktop?wid=829&hei=455&dpr=off' },
+    { id: 11, name: 'Donut', category: 'Desert', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-toffee-apple-doughnut:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 12, name: 'Apple Pie', category: 'Desert', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-ApplePie:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 13, name: 'Cookie', category: 'Desert', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Triple-Chocolate-Cookie-2:product-header-desktop?wid=829&hei=455&dpr=off' },
+    { id: 14, name: 'Muffin', category: 'Desert', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Mixed-Berry-Muffin-Xmas-2:product-header-desktop?wid=829&hei=455&dpr=off'},
+    { id: 15, name: 'Brownie', category: 'Desert', likes: 0, imageUrl: 'https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-ChocolateBrownie:product-header-desktop?wid=829&hei=455&dpr=off' },
+    ]
+}
 }
